c124367edf16cb0f59647c3ae88f1dd5420f007e
Ramoschriss <ramoschris901@gmail.com>
2024-12-13 19:27:39 +0000

    Ajustes

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -39,11 +39,7 @@ class MainActivity : AppCompatActivity() {
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
 
-                // Navegar para a proxima tela
-                // Criar o layout da proxima tela
-                // Passar os dados (resultados) para a proxima tela
 
-                // Intent - Classe do proprio android
 
                 val intent = Intent(this, ResultActivity::class.java)
                 intent.putExtra(key_result_IMC, resultado)
